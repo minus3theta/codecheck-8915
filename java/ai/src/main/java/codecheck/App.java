7c4d2ae28d8b70cfe272198190875b41b728bf6b
minus3theta <yone_j_synthesis@yahoo.co.jp>
2017-04-10 02:21:25 +0900

    Implement ai

--- java/ai/src/main/java/codecheck/App.java
+++ java/ai/src/main/java/codecheck/App.java
@@ -1,9 +1,37 @@
 package codecheck;
 
+import java.util.LinkedList;
+
 public class App {
-	public static void main(String[] args) {
-		for (int i = 0, l = args.length; i < l; i++) {
-			System.out.println(args[i]);
-		}
-	}
+    public static void main(String[] args) {
+        if(args.length < 1) {
+            throw new IllegalArgumentException("At least 1 argument is required");
+        }
+        int currentLast = last(args[0]);
+        LinkedList<String> words = new LinkedList<String>();
+        for(int i=1; i<args.length; i++) {
+            words.addLast(args[i]);
+        }
+        for(String s: words) {
+            if(first(s) == currentLast) {
+                System.out.println(s);
+                return;
+            }
+        }
+    }
+
+    private static int first(String s) {
+        return s.codePointAt(0);
+    }
+
+    private static int last(String s) {
+        int l = s.length();
+        if(l < 2) {
+            return s.codePointAt(l - 1);
+        }
+        if(Character.isSurrogatePair(s.charAt(l - 2), s.charAt(l - 1))) {
+            return s.codePointAt(l - 2);
+        }
+        return s.codePointAt(l - 1);
+    }
 }
