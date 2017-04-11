749eaae4012b33acd7cfa9e07c73fa6baa7f9db0
minus3theta <yone_j_synthesis@yahoo.co.jp>
2017-04-11 23:14:48 +0900

    Build a graph of words

--- java/ai/src/main/java/codecheck/App.java
+++ java/ai/src/main/java/codecheck/App.java
@@ -1,6 +1,7 @@
 package codecheck;
 
 import java.util.LinkedList;
+import java.util.HashMap;
 
 public class App {
     public static void main(String[] args) {
@@ -11,6 +12,22 @@ public class App {
         LinkedList<String> words = new LinkedList<String>();
         for(int i=1; i<args.length; i++) {
             words.addLast(args[i]);
+        }
+        HashMap<Integer, HashMap<Integer, Integer>> graph =
+            new HashMap<Integer, HashMap<Integer, Integer>>();
+        for(String s: words) {
+            int first = first(s);
+            int last = last(s);
+            HashMap<Integer, Integer> val = graph.get(first);
+            if(val == null) {
+                val = new HashMap<Integer, Integer>();
+                graph.put(first, val);
+            }
+            Integer count = val.get(last);
+            if(count == null) {
+                count = 0;
+            }
+            val.put(last, count + 1);
         }
         for(String s: words) {
             if(first(s) == currentLast) {
