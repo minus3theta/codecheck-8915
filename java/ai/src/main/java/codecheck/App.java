a2dd0dd1ca642b319392a89faf7658cc06a21e55
minus3theta <yone_j_synthesis@yahoo.co.jp>
2017-04-12 03:42:30 +0900

    Implement DFS AI

--- java/ai/src/main/java/codecheck/App.java
+++ java/ai/src/main/java/codecheck/App.java
@@ -1,7 +1,9 @@
 package codecheck;
 
 import java.util.LinkedList;
+import java.util.Map;
 import java.util.HashMap;
+import java.util.OptionalInt;
 
 public class App {
     public static void main(String[] args) {
@@ -9,32 +11,94 @@ public class App {
             throw new IllegalArgumentException("At least 1 argument is required");
         }
         int currentLast = last(args[0]);
-        LinkedList<String> words = new LinkedList<String>();
+        LinkedList<String> words = new LinkedList<>();
         for(int i=1; i<args.length; i++) {
             words.addLast(args[i]);
         }
-        HashMap<Integer, HashMap<Integer, Integer>> graph =
-            new HashMap<Integer, HashMap<Integer, Integer>>();
+        HashMap<Integer, HashMap<Integer, Integer>> graph = new HashMap<>();
         for(String s: words) {
             int first = first(s);
             int last = last(s);
-            HashMap<Integer, Integer> val = graph.get(first);
-            if(val == null) {
-                val = new HashMap<Integer, Integer>();
-                graph.put(first, val);
-            }
-            Integer count = val.get(last);
-            if(count == null) {
-                count = 0;
-            }
+            HashMap<Integer, Integer> val =
+                graph.computeIfAbsent(first, k -> new HashMap<>());
+            int count = val.getOrDefault(last, 0);
             val.put(last, count + 1);
         }
+        int hand = win(currentLast, graph)
+            .orElseGet(() -> {
+                    HashMap<Integer, Integer> val = graph.get(currentLast);
+                    if(val == null) return 0;
+                    return val.keySet().iterator().next();
+                });
         for(String s: words) {
-            if(first(s) == currentLast) {
+            if(last(s) == hand) {
                 System.out.println(s);
                 return;
             }
         }
+    }
+
+    private static String cpToString(int cp) {
+        return new String(new int[]{cp}, 0, 1);
+    }
+
+    private static void show(HashMap<Integer, HashMap<Integer, Integer>> graph) {
+        graph.forEach((fst,m) ->
+                      m.forEach((lst,cnt) ->
+                                System.err.println(new String(new int[]{fst}, 0, 1) +
+                                                   " -> " +
+                                                   new String(new int[]{lst}, 0, 1) + " : " + cnt)));
+    }
+
+    private static OptionalInt win(int current, HashMap<Integer, HashMap<Integer, Integer>> graph) {
+        HashMap<Integer, Integer> candidate = graph.get(current);
+        if(candidate == null) {
+            return OptionalInt.empty();
+        }
+        for(Map.Entry<Integer, Integer> e: candidate.entrySet()) {
+            int last = e.getKey();
+            int count = e.getValue();
+            if(count == 0) {
+                continue;
+            }
+            HashMap<Integer, HashMap<Integer, Integer>> newGraph =
+                deepClone(graph);
+            HashMap<Integer, Integer> newCandidate = newGraph.get(current);
+            newCandidate.compute(last, (k,c) -> c - 1);
+            if(lose(last, newGraph)) {
+                return OptionalInt.of(last);
+            }
+        }
+        return OptionalInt.empty();
+    }
+
+    private static boolean lose(int current, HashMap<Integer, HashMap<Integer, Integer>> graph) {
+        HashMap<Integer, Integer> candidate = graph.get(current);
+        if(candidate == null) {
+            return true;
+        }
+        for(Map.Entry<Integer, Integer> e: candidate.entrySet()) {
+            int last = e.getKey();
+            int count = e.getValue();
+            if(count == 0) {
+                continue;
+            }
+            HashMap<Integer, HashMap<Integer, Integer>> newGraph =
+                deepClone(graph);
+            HashMap<Integer, Integer> newCandidate = newGraph.get(current);
+            newCandidate.compute(last, (k,c) -> c - 1);
+            if(!win(last, newGraph).isPresent()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static HashMap<Integer, HashMap<Integer, Integer>>
+        deepClone(HashMap<Integer, HashMap<Integer, Integer>> graph) {
+        HashMap<Integer, HashMap<Integer, Integer>> ret = new HashMap<>();
+        graph.forEach((k,v) -> ret.put(k,(HashMap<Integer, Integer>)v.clone()));
+        return ret;
     }
 
     private static int first(String s) {
